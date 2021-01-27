6676b3b0e67179c0635f47eacbd231407192338d
Asrulramadani <68553467+Asrulramadani@users.noreply.github.com>
2021-01-27 09:39:56 +0800

    Add files via upload

--- asset/main.js
+++ asset/main.js
@@ -20,16 +20,17 @@ window.onload = function () {
     })
 }
 
-const nama = prompt("Masukkan Nama Panggilan Anda : ");
 
 
-function createBubble() {
+
+function createImg() {
     const section = document.querySelector("section");
-    let createElement = document.createElement("span");
+    let createElement = document.createElement("img");
     let size = Math.random() * 60;
 
-    createElement.style.width = 20 + size +'px';
-    createElement.style.height = 20 + size +'px';
+    createElement.setAttribute("src","asset/1.png");
+    createElement.style.width = 10 + size +'px';
+    createElement.style.height = 10 + size +'px';
     createElement.style.left = Math.random() * window.innerWidth +'px';
     createElement.style.animationDuration = Math.random() * 10 + 4 + 's';
 
@@ -37,37 +38,42 @@ function createBubble() {
 
     setTimeout(()=>{
         createElement.remove();
-    },5000)
+    },4000)
 }
 
 setInterval(() => {
-    createBubble();
-}, 120);
-
+    createImg();
+}, 200);
 
 
 function sapa() {
     const h1 = document.querySelector("h1");
+    const img = document.getElementsByName('img');
+    const root= document.querySelector(':root');
     let jam = new Date().getHours();
 
     if(jam > 18 ) {
-        h1.innerHTML = `Hi, ${nama}`
-    }
-    else if(jam > 15) {
-        h1.innerHTML = `Hi, ${nama}`
+        h1.innerHTML = `Good Night <img src="asset/2.png" class="love">`;
+        root.style.setProperty('--primary-color',' rgba(255,255,255,.9');
+        root.style.setProperty('--secondary-color',' rgb(5, 5, 54)');
+        
     }
     else if(jam > 11) {
-        h1.innerHTML = `Hi, ${nama}`
+        h1.innerHTML = `Good Afternoon <img src="asset/2.png" class="love">`;
+        root.style.setProperty('--primary-color',' rgb(255, 174, 0)');
+        root.style.setProperty('--secondary-color','rgba(255,255,255,.9');      
     }
     else {
-        h1.innerHTML = `Hi, ${nama}`
+        h1.innerHTML = `Good Morning <img src="asset/2.png" class="love">`;
+        root.style.setProperty('--primary-color','rgb(0, 89, 206)');
+        root.style.setProperty('--secondary-color','rgba(255,255,255,.9');
     }
 }
 
 sapa();
 
 
-function waktu() {
+function jam() {
     let jam = new Date().getHours();
     let menit = new Date().getMinutes();
     let detik = new Date().getSeconds();
@@ -82,5 +88,5 @@ function waktu() {
 }
 
 setInterval(()=>{
-    waktu();
+    jam();
 },1000)
